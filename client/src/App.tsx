f5d829b6116e57b00541c0fe648ed5d669d3452f
Jonathan Steen <therightjon@gmail.com>
2026-02-18 13:15:38 -0600

    Refactor toast notifications: replace SonnerToaster with custom useToast hook and adjust toast duration settings

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import { Switch, Route, useLocation } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/shared/components/ui/toaster";
-import { Toaster as SonnerToaster } from "sonner";
 import { TooltipProvider } from "@/shared/components/ui/tooltip";
 import { AuthProvider } from "@/features/auth/hooks/use-auth";
 import { ThemeProvider } from "@/shared/components/layout/theme-provider";
@@ -238,7 +237,6 @@ function App() {
             <SessionExpirationHandler />
             <TooltipProvider>
               <Toaster />
-              <SonnerToaster richColors />
               <Router />
             </TooltipProvider>
           </AuthProvider>
